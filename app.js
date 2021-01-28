abb7e092c82b0df8c69c5dd593bbbaf814411973
Pavle Marinkovic <payche255@gmail.com>
2021-01-28 19:02:54 +0100

    commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const btnHamburger = document.querySelector("#btnHamburger");
-
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
@@ -10,6 +10,7 @@ btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     // Close Hamburger Menu
 
+    body.classList.remove("noscroll");
     header.classList.remove("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-in");
@@ -17,6 +18,7 @@ btnHamburger.addEventListener("click", function () {
     });
   } else {
     // Open Hamburger Menu
+    body.classList.add("noscroll");
     header.classList.add("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-out");
